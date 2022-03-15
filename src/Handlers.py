f06c40f9ca6b9b118b8e25acdfcfb105b7f18664
Sejin Park <spark990605@gmail.com>
2022-03-15 10:22:59 +0900

    Add Event Handlers

---
+++ src/Handlers.py
@@ -0,0 +1,33 @@
+from tkinter import filedialog as fd
+from Logic import editHWP
+
+def onHWPButtonClick():
+    filetypes = (
+        ('한글 파일', '*.hwp, *.hwpx'),
+        ('모든 파일', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title = "한글 파일 불러오기",
+        initialdir = '/',
+        filetypes = filetypes
+    )
+
+    return filename
+
+def onExcelButtonClick():
+        filetypes = (
+            ('엑셀 파일', '*.xlsx'),
+            ('모든 파일', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title = "엑셀 파일 불러오기",
+            initialdir = '/',
+            filetypes = filetypes
+        )
+
+        return filename
+
+def onMacroButtonClick(excelPath, hwpPath):
+    editHWP(excelPath, hwpPath)
