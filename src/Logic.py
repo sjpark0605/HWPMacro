c8356c44451db7d060877b01c92feaa6023ba8f0
Sejin Park <spark990605@gmail.com>
2022-03-15 09:40:14 +0900

    Refactor

---
+++ src/Logic.py
@@ -0,0 +1,35 @@
+import win32com.client as win32 
+import pandas as pd 
+
+def editHWP(excelPath, hwpFile):
+
+    excel = pd.read_excel(excelPath)
+    print(excel)
+
+    hwp = win32.Dispatch("HWPFrame.HwpObject")
+    hwp.Open(hwpFile)
+
+    field_list = [i for i in hwp.GetFieldList().split("\x02")]
+    print(field_list)
+
+    # Select and Copy All Text
+    hwp.Run('SelectAll')
+    hwp.Run('Copy')
+
+    # Move Cursor to End of Document
+    hwp.MovePos(3)
+
+    for i in range(len(excel) - 1):
+        # Paste Previously Copied Text
+        hwp.Run('Paste')
+
+        # Move Cursor to End of Document
+        hwp.MovePos(3)
+
+    for page in range(len(excel)):
+        for field in field_list:
+            # Replace Fields with Data in Fields
+            hwp.PutFieldText(f'{field}{{{{{page}}}}}', excel[field].iloc[page])
+
+    hwp.Save()
+    hwp.Quit()
